dcf690128baceb66d893363eed04441d484b2f00
fkthat <fkthat@yandex.ru>
2023-01-12 14:21:34 +0300

    Replace RandomGenExtensions with interface methods.

--- src/FkThat.Mockables/IRandomGen.cs
+++ src/FkThat.Mockables/IRandomGen.cs
@@ -1,3 +1,5 @@
+using System.Runtime.InteropServices;
+
 namespace FkThat.Mockables;
 
 /// <summary>
@@ -10,4 +12,102 @@ public interface IRandomGen
     /// </summary>
     /// <param name="data">The span to fill with random bytes.</param>
     void GetBytes(Span<byte> data);
+
+    /// <summary>
+    /// Fills an array with random bytes.
+    /// </summary>
+    /// <param name="data">The array to fill.</param>
+    /// <param name="offset">The index of the array to start the fill operation.</param>
+    /// <param name="count">The number of bytes to fill.</param>
+    void GetBytes(byte[] data, int offset, int count)
+    {
+        _ = data ?? throw new ArgumentNullException(nameof(data));
+
+        if (offset < 0 || offset > data.Length - 1)
+        {
+            throw new ArgumentOutOfRangeException(nameof(offset));
+        }
+
+        if (count < 0 || count > data.Length - offset)
+        {
+            throw new ArgumentOutOfRangeException(nameof(count));
+        }
+
+        Span<byte> span = new(data, offset, count);
+        GetBytes(span);
+    }
+
+    /// <summary>
+    /// Fills an array with random bytes.
+    /// </summary>
+    /// <param name="data">The array to fill.</param>
+    void GetBytes(byte[] data)
+    {
+        _ = data ?? throw new ArgumentNullException(nameof(data));
+        GetBytes(data, 0, data.Length);
+    }
+
+    /// <summary>
+    /// Creates an array of bytes with a random sequence of values.
+    /// </summary>
+    /// <param name="count">The number of bytes of random values to create.</param>
+    /// <returns></returns>
+    byte[] GetBytes(int count)
+    {
+        if (count < 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(count));
+        }
+
+        var data = new byte[count];
+        GetBytes(data);
+        return data;
+    }
+
+    /// <summary>
+    /// Returns a non-negative random <c cref="int"/> less than the specified maximum.
+    /// </summary>
+    /// <param name="toExclusive">The exclusive upper bound of the random number returned.</param>
+    int GetInt32(int toExclusive)
+    {
+        if (toExclusive <= 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(toExclusive));
+        }
+
+        var mask = (uint)toExclusive - 1;
+        mask |= mask >> 1;
+        mask |= mask >> 2;
+        mask |= mask >> 4;
+        mask |= mask >> 8;
+        mask |= mask >> 16;
+
+        uint result = 0;
+        Span<byte> span = MemoryMarshal.AsBytes(new Span<uint>(ref result));
+        while (true)
+        {
+            GetBytes(span);
+            result &= mask;
+
+            if (result < toExclusive)
+            {
+                return (int)result;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Returns a random <c cref="int"/> within a specified range.
+    /// </summary>
+    /// <param name="fromInclusive">The inclusive lower bound of the random number returned.</param>
+    /// <param name="toExclusive">The exclusive upper bound of the random number returned.</param>
+    int GetInt32(int fromInclusive, int toExclusive)
+    {
+        if (toExclusive <= fromInclusive)
+        {
+            throw new ArgumentOutOfRangeException(nameof(toExclusive));
+        }
+
+        return GetInt32(toExclusive - fromInclusive) + fromInclusive;
+    }
 }
--- test/Tests.FkThat.Mockables/Test_IRandomGen.cs
+++ test/Tests.FkThat.Mockables/Test_IRandomGen.cs
@@ -1,25 +1,16 @@
 namespace FkThat.Mockables;
 
-public class Test_RandomGenExtensions
+public class Test_IRandomGen
 {
     // GetBytes(data, offset, count)
 
     [Fact]
-    public void GetBytes_with_data_offset_count_should_check_null_random()
-    {
-        IRandomGen random = null!;
-
-        FluentActions.Invoking(() => random.GetBytes(new byte[4], 0, 4))
-            .Should().Throw<ArgumentNullException>().Which.ParamName
-            .Should().Be(nameof(random));
-    }
-
-    [Fact]
     public void GetBytes_with_data_offset_count_should_check_null_data()
     {
-        IRandomGen random = A.Fake<IRandomGen>();
+        IRandomGen sut = A.Fake<FakeRandomGen>();
         byte[] data = null!;
-        FluentActions.Invoking(() => random.GetBytes(data, 0, 4))
+
+        FluentActions.Invoking(() => sut.GetBytes(data, 0, 4))
             .Should().Throw<ArgumentNullException>().Which.ParamName
             .Should().Be(nameof(data));
     }
@@ -27,18 +18,17 @@ public class Test_RandomGenExtensions
     [Fact]
     public void GetBytes_with_data_offset_count_should_check_offset()
     {
-        IRandomGen random = A.Fake<IRandomGen>();
-        int offset;
+        IRandomGen sut = A.Fake<FakeRandomGen>();
 
         // negative offset
-        offset = -1;
-        FluentActions.Invoking(() => random.GetBytes(new byte[4], offset, 4))
+        int offset = -1;
+        FluentActions.Invoking(() => sut.GetBytes(new byte[4], offset, 4))
             .Should().Throw<ArgumentOutOfRangeException>().Which.ParamName
             .Should().Be(nameof(offset));
 
         // too large offset
         offset = 4;
-        FluentActions.Invoking(() => random.GetBytes(new byte[4], offset, 4))
+        FluentActions.Invoking(() => sut.GetBytes(new byte[4], offset, 4))
             .Should().Throw<ArgumentOutOfRangeException>().Which.ParamName
             .Should().Be(nameof(offset));
     }
@@ -46,18 +36,17 @@ public class Test_RandomGenExtensions
     [Fact]
     public void GetBytes_with_data_offset_count_should_check_count()
     {
-        IRandomGen random = A.Fake<IRandomGen>();
-        int count;
+        IRandomGen sut = A.Fake<FakeRandomGen>();
 
         // negative count
-        count = -1;
-        FluentActions.Invoking(() => random.GetBytes(new byte[4], 0, count))
+        int count = -1;
+        FluentActions.Invoking(() => sut.GetBytes(new byte[4], 0, count))
             .Should().Throw<ArgumentOutOfRangeException>().Which.ParamName
             .Should().Be(nameof(count));
 
         // too large count
         count = 5;
-        FluentActions.Invoking(() => random.GetBytes(new byte[4], 0, count))
+        FluentActions.Invoking(() => sut.GetBytes(new byte[4], 0, count))
             .Should().Throw<ArgumentOutOfRangeException>().Which.ParamName
             .Should().Be(nameof(count));
     }
@@ -67,30 +56,21 @@ public class Test_RandomGenExtensions
     {
         var data = new byte[] { 16, 112, 193, 67 };
         var random = A.Fake<FakeRandomGen>();
-        A.CallTo(() => random.GetBytes(2)).Returns(new byte[] { 42, 73 });
-        RandomGenExtensions.GetBytes(random, data, 1, 2);
+        A.CallTo(() => random.GetFakeBytes(2)).Returns(new byte[] { 42, 73 });
+        IRandomGen sut = random;
+        sut.GetBytes(data, 1, 2);
         data.Should().Equal(new byte[] { 16, 42, 73, 67 });
     }
 
     // GetBytes(data)
 
     [Fact]
-    public void GetBytes_with_data_should_check_null_random()
-    {
-        IRandomGen random = null!;
-
-        FluentActions.Invoking(() => RandomGenExtensions.GetBytes(random, new byte[4]))
-            .Should().Throw<ArgumentNullException>().Which.ParamName
-            .Should().Be(nameof(random));
-    }
-
-    [Fact]
     public void GetBytes_with_data_should_check_null_data()
     {
-        IRandomGen random = A.Fake<IRandomGen>();
+        IRandomGen sut = A.Fake<FakeRandomGen>();
         byte[] data = null!;
 
-        FluentActions.Invoking(() => RandomGenExtensions.GetBytes(random, data))
+        FluentActions.Invoking(() => sut.GetBytes(data))
             .Should().Throw<ArgumentNullException>().Which.ParamName
             .Should().Be(nameof(data));
     }
@@ -100,29 +80,21 @@ public class Test_RandomGenExtensions
     {
         var data = new byte[] { 16, 112, 193, 67 };
         var random = A.Fake<FakeRandomGen>();
-        A.CallTo(() => random.GetBytes(4)).Returns(new byte[] { 235, 55, 221, 159 });
-        RandomGenExtensions.GetBytes(random, data);
+        A.CallTo(() => random.GetFakeBytes(4)).Returns(new byte[] { 235, 55, 221, 159 });
+        IRandomGen sut = random;
+        sut.GetBytes(data);
         data.Should().Equal(new byte[] { 235, 55, 221, 159 });
     }
 
     // GetBytes(count)
 
     [Fact]
-    public void GetBytes_with_count_should_check_null_random()
-    {
-        IRandomGen random = null!;
-
-        FluentActions.Invoking(() => random.GetBytes(4))
-            .Should().Throw<ArgumentNullException>().Which.ParamName
-            .Should().Be(nameof(random));
-    }
-
-    [Fact]
     public void GetBytes_with_count_should_check_count()
     {
-        var random = A.Fake<IRandomGen>();
+        IRandomGen sut = A.Fake<FakeRandomGen>();
         int count = -1;
-        FluentActions.Invoking(() => random.GetBytes(count))
+
+        FluentActions.Invoking(() => sut.GetBytes(count))
             .Should().Throw<ArgumentOutOfRangeException>().Which.ParamName
             .Should().Be(nameof(count));
     }
@@ -131,37 +103,29 @@ public class Test_RandomGenExtensions
     public void GetBytes_with_count_should_return_data()
     {
         var random = A.Fake<FakeRandomGen>();
-        A.CallTo(() => random.GetBytes(4)).Returns(new byte[] { 235, 55, 221, 159 });
-        var actual = RandomGenExtensions.GetBytes(random, 4);
+
+        A.CallTo(() => random.GetFakeBytes(4))
+            .Returns(new byte[] { 235, 55, 221, 159 });
+
+        IRandomGen sut = random;
+        var actual = sut.GetBytes(4);
         actual.Should().Equal(new byte[] { 235, 55, 221, 159 });
     }
 
     // GetInt32(toExclusive)
 
     [Fact]
-    public void GetInt32_with_toExclusive_should_check_null_random()
-    {
-        IRandomGen random = null!;
-
-        FluentActions.Invoking(() => random.GetInt32(42))
-            .Should().Throw<ArgumentNullException>().Which.ParamName
-            .Should().Be(nameof(random));
-    }
-
-    [Fact]
     public void GetInt32_with_toExclusive_should_check_toExclusive()
     {
-        IRandomGen random = A.Fake<IRandomGen>();
+        IRandomGen sut = A.Fake<FakeRandomGen>();
 
-        int toExclusive;
-
-        toExclusive = 0;
-        FluentActions.Invoking(() => random.GetInt32(toExclusive))
+        int toExclusive = 0;
+        FluentActions.Invoking(() => sut.GetInt32(toExclusive))
             .Should().Throw<ArgumentOutOfRangeException>().Which.ParamName
             .Should().Be(nameof(toExclusive));
 
         toExclusive = -1;
-        FluentActions.Invoking(() => random.GetInt32(toExclusive))
+        FluentActions.Invoking(() => sut.GetInt32(toExclusive))
             .Should().Throw<ArgumentOutOfRangeException>().Which.ParamName
             .Should().Be(nameof(toExclusive));
     }
@@ -171,41 +135,30 @@ public class Test_RandomGenExtensions
     {
         var random = A.Fake<FakeRandomGen>();
 
-        A.CallTo(() => random.GetBytes(4))
+        A.CallTo(() => random.GetFakeBytes(4))
             .Returns(new byte[] { 0xff, 0xff, 0xff, 0xff }).Once().Then
             .Returns(new byte[] { 0x44, 0x8a, 0x6d, 0x13 }).Once().Then
             .Returns(new byte[] { 0x00, 0x00, 0x00, 0x00 });
 
-        var actual = random.GetInt32(42);
+        IRandomGen sut = random;
+        var actual = sut.GetInt32(42);
         actual.Should().Be(4);
     }
 
     // GetInt32(fromInclusive, toExclusive)
 
     [Fact]
-    public void GetInt32_with_fromInclusive_toExclusive_should_check_null_random()
-    {
-        IRandomGen random = null!;
-
-        FluentActions.Invoking(() => random.GetInt32(42, 73))
-            .Should().Throw<ArgumentNullException>().Which.ParamName
-            .Should().Be(nameof(random));
-    }
-
-    [Fact]
     public void GetInt32_with_fromInclusive_toExclusive_should_check_toExclusive()
     {
-        IRandomGen random = A.Fake<IRandomGen>();
+        IRandomGen sut = A.Fake<FakeRandomGen>();
 
-        int toExclusive;
-
-        toExclusive = 42;
-        FluentActions.Invoking(() => random.GetInt32(42, toExclusive))
+        int toExclusive = 42;
+        FluentActions.Invoking(() => sut.GetInt32(42, toExclusive))
             .Should().Throw<ArgumentOutOfRangeException>().Which.ParamName
             .Should().Be(nameof(toExclusive));
 
         toExclusive = 41;
-        FluentActions.Invoking(() => random.GetInt32(42, toExclusive))
+        FluentActions.Invoking(() => sut.GetInt32(42, toExclusive))
             .Should().Throw<ArgumentOutOfRangeException>().Which.ParamName
             .Should().Be(nameof(toExclusive));
     }
@@ -215,22 +168,22 @@ public class Test_RandomGenExtensions
     {
         var random = A.Fake<FakeRandomGen>();
 
-        A.CallTo(() => random.GetBytes(4))
+        A.CallTo(() => random.GetFakeBytes(4))
             .Returns(new byte[] { 0xff, 0xff, 0xff, 0xff }).Once().Then
             .Returns(new byte[] { 0x44, 0x8a, 0x6d, 0x13 }).Once().Then
             .Returns(new byte[] { 0x00, 0x00, 0x00, 0x00 });
 
-        var actual = random.GetInt32(42, 73);
+        IRandomGen sut = random;
+        var actual = sut.GetInt32(42, 73);
         actual.Should().Be(46);
     }
+}
 
-    // Fake helper
+// Fake helper
+file abstract class FakeRandomGen : IRandomGen
+{
+    public void GetBytes(Span<byte> data) =>
+        new Span<byte>(GetFakeBytes(data.Length)).CopyTo(data);
 
-    internal abstract class FakeRandomGen : IRandomGen
-    {
-        public void GetBytes(Span<byte> data) =>
-            new Span<byte>(GetBytes(data.Length)).CopyTo(data);
-
-        public abstract byte[] GetBytes(int count);
-    }
+    public abstract byte[] GetFakeBytes(int count);
 }
