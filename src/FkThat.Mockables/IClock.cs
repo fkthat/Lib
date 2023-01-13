de80358a0dd738b952157342c216dbcc16d99d6b
fkthat <fkthat@yandex.ru>
2023-01-13 04:46:26 +0300

    Update IClock and SystemClock. - Add the "TimeZone" property. - Add the "Now" property.

--- src/FkThat.Mockables/IClock.cs
+++ src/FkThat.Mockables/IClock.cs
@@ -6,12 +6,40 @@ namespace FkThat.Mockables;
 public interface IClock
 {
     /// <summary>
-    /// Gets a System.DateTimeOffset object whose date and time are set to the current Coordinated
-    /// Universal Time (UTC) date and time and whose offset is System.TimeSpan.Zero.
+    /// Gets a <c cref="DateTimeOffset"/> object whose date and time are set to the current
+    /// Coordinated Universal Time (UTC) date and time and whose offset is <c c="TimeSpan.Zero"/>.
     /// </summary>
     /// <value>
     /// An object whose date and time is the current Coordinated Universal Time (UTC) and whose
     /// offset is System.TimeSpan.Zero.
     /// </value>
     DateTimeOffset UtcNow { get; }
+
+    /// <summary>
+    /// Gets a <c cref="TimeZoneInfo"/> object that represents the local time zone.
+    /// </summary>
+    /// <value>An object that represents the local time zone.</value>
+    TimeZoneInfo TimeZone { get; }
+
+    /// <summary>
+    /// Gets a <c cref="DateTimeOffset"/> object that is set to the current date and time on the
+    /// current computer, with the offset set to the local time's offset from Coordinated Universal
+    /// Time (UTC).
+    /// </summary>
+    /// <value>
+    /// A <c cref="DateTimeOffset"/> object whose date and time is the current local time and whose
+    /// offset is the local time zone's offset from Coordinated Universal Time (UTC).
+    /// </value>
+    DateTimeOffset Now
+    {
+        get
+        {
+            var utcNow = UtcNow;
+            var offset = TimeZone.GetUtcOffset(utcNow);
+
+            return new DateTimeOffset(
+                utcNow.Ticks + offset.Ticks,
+                TimeSpan.FromTicks(offset.Ticks));
+        }
+    }
 }
---
+++ test/Tests.FkThat.Mockables/Test_IClock.cs
@@ -0,0 +1,37 @@
+﻿namespace FkThat.Mockables;
+
+public class Test_IClock
+{
+    [Fact]
+    public void Now_should_return_local_time()
+    {
+        var clock = A.Fake<FakeClock>();
+        IClock sut = clock;
+
+        var tz = TimeZoneInfo.FindSystemTimeZoneById("W. Europe Standard Time");
+        A.CallTo(() => clock.TimeZone).Returns(tz);
+
+        // just before the summer time transition
+        var utcNow = new DateTimeOffset(2023, 3, 26, 0, 0, 59, TimeSpan.Zero);
+        A.CallTo(() => clock.UtcNow).Returns(utcNow);
+
+        // should be +1h
+        sut.Now.Should().Be(new DateTimeOffset(
+            2023, 3, 26, 1, 0, 59, TimeSpan.FromHours(1)));
+
+        // just after the summer time transition
+        utcNow = new DateTimeOffset(2023, 3, 26, 1, 0, 1, TimeSpan.Zero);
+        A.CallTo(() => clock.UtcNow).Returns(utcNow);
+
+        // should be +2h
+        sut.Now.Should().Be(new DateTimeOffset(
+            2023, 3, 26, 3, 0, 1, TimeSpan.FromHours(2)));
+    }
+}
+
+file abstract class FakeClock : IClock
+{
+    public abstract DateTimeOffset UtcNow { get; }
+
+    public abstract TimeZoneInfo TimeZone { get; }
+}
